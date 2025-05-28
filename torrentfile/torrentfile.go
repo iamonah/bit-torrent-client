62c2ca9a7fee8fddd261a57a7feb6e127cff5a30
onahvictor <onahvictorc@gmail.com>
2025-05-28 13:12:30 +0100

    bedecoding of torrent file

---
+++ torrentfile/torrentfile.go
@@ -0,0 +1,94 @@
+package torrentfile
+
+import (
+	"bytes"
+	"crypto/sha1"
+	"fmt"
+	"os"
+
+	"github.com/jackpal/bencode-go"
+)
+
+
+type bencodeTorrent struct {
+	Announce string       `bencode:"announce"`
+	Info     bencodedInfo `bencode:"info"`
+}
+
+type bencodedFileInfo struct { // multi file data
+	Length int      `bencode:"length"`
+	Path   []string `bencode:"path"`
+}
+
+type bencodedInfo struct {
+	Pieces      string             `bencode:"pieces"` //a slices of pieces
+	PieceLength int                `bencode:"piece lenght"`
+	Name        string             `bencode:"name"`
+	Length      int                `bencode:"length,omitempty"` //single file
+	Files       []bencodedFileInfo `bencode:"files,omitempty"`  //multi file
+}
+
+func (i *bencodedInfo) hash() ([20]byte, error) {
+	var buf bytes.Buffer
+	err := bencode.Marshal(&buf, *i)
+	if err != nil {
+		return [20]byte{}, err
+	}
+
+	h := sha1.Sum(buf.Bytes())
+	return h, nil
+}
+
+func (i *bencodedInfo) splitPieceHashes() ([][20]byte, error) {
+	hashLen := 20 //len of SHA-1 hashes
+	buf := []byte(i.Pieces)
+
+	if len(buf)%hashLen != 0 {
+		err := fmt.Errorf("recieved malformed piece of length %d", len(buf))
+		return nil, err
+	}
+
+	var hashPieces [][20]byte
+
+	for i := 0; i < len(buf); i += 20 {
+		chunk := buf[i : i+20]
+		hashPieces = append(hashPieces, [20]byte(chunk))
+	}
+	return hashPieces, nil
+}
+
+// torrent file is bencoded
+func Open(path string) (*bencodeTorrent, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	torrentInfo := bencodeTorrent{}
+	err = bencode.Unmarshal(file, &torrentInfo)
+	if err != nil {
+		return nil, err
+	}
+	return &torrentInfo, nil
+}
+
+func (bto *bencodeTorrent) toTorrentFile() (*TorrentFile, error) {
+	infoHash, err := bto.Info.hash()
+	if err != nil {
+		return nil, err
+	}
+	hash, err := bto.Info.splitPieceHashes()
+	if err != nil {
+		return nil, err
+	}
+	t := TorrentFile{
+		Announce:     bto.Announce,
+		InfoHash:     infoHash,
+		PieceLength:  bto.Info.Length,
+		Name:         bto.Info.Name,
+		Length:       bto.Info.Length,
+		PiecesHashes: hash,
+	}
+	return &t, nil
+}
